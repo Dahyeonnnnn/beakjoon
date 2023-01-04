86f4e7b33a3a0293190c2c02e188687d179ce87e
andahyeon <adh0618@gmail.com>
2023-01-04 16:59:07 +0900

    백준 단계별 연습문제

---
+++ src/exercise/ex2557.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.io.Console;
+
+public class ex2557 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world!");
+		
+	}
+
+}
+
