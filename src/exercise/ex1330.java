87659bd6f30f636b7a3ea8e97d74a4f710fa2e3f
andahyeon <adh0618@gmail.com>
2023-01-09 17:38:23 +0900

    추가

--- src/exercise/ex1330.java
+++ src/exercise/ex1330.java
@@ -1,10 +1,23 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class ex1330 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		int a= scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		if(a>b) {
+			System.out.println(">");
+		}else if (a==b) {
+			System.out.println("==");
+		}else{
+			System.out.println("<");
+		}
 	}
 
 }
