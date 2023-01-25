7f806f58715e53c29616c300054c614d1da7ecd9
andahyeon <adh0618@gmail.com>
2023-01-25 18:05:34 +0900

    추가

--- src/exercise/ex1546.java
+++ src/exercise/ex1546.java
@@ -1,10 +1,38 @@
 package exercise;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ex1546 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		int[] arr= new int[a];
+		
+		for(int i=0;i<a;i++) {
+			
+			
+			arr[i] = scanner.nextInt();
+			
+			
+		}
+		Arrays.sort(arr);
+		
+		
+		double sum=0;
+		
+		for(int i=0;i<a;i++) {
+			sum=arr[i]/arr[arr.length-1]*100;
+		
+		}
+		double result=sum/a;
+		
+		System.out.println(result);
 	}
 
 }
