e327a7019c9c27c617cd1db104cef7f85a1660df
andahyeon <adh0618@gmail.com>
2023-02-14 17:15:32 +0900

    추가

--- src/exercise/ex2941.java
+++ src/exercise/ex2941.java
@@ -1,11 +1,31 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class ex2941 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner
+		Scanner scanner = new Scanner(System.in);
+
+		String a = scanner.next();
+
+		int count = 0;
+
+		for (int i = 0; i < a.length(); i++) {
+
+			char ch = a.charAt(i);
+
+			if (ch == 'c') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == '=') {
+						i++;
+					}
+				}
+			}
+		}
+
 	}
 
 }
