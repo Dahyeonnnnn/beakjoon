5f1260b33fbfb40da5bfdd62d0834e58877c2471
andahyeon <adh0618@gmail.com>
2023-02-15 17:19:55 +0900

    추가

--- src/exercise/ex2941.java
+++ src/exercise/ex2941.java
@@ -21,10 +21,64 @@ public class ex2941 {
 				if (i < a.length() - 1) {
 					if (a.charAt(i + 1) == '=') {
 						i++;
+					} else if (a.charAt(i + 1) == '-') {
+						i++;
 					}
 				}
 			}
+
+			else if (ch == 'd') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == 'z') {
+						if (i < a.length() - 2) {
+							if (a.charAt(i + 2) == '=') { // dz= 일 경우
+								i += 2;
+							}
+						}
+					}
+
+					else if (a.charAt(i + 1) == '-') { // d- 일 경우
+						i++;
+					}
+				}
+			}
+
+			else if (ch == 'l') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == 'j') { // lj 일 경우
+						i++;
+					}
+				}
+			}
+
+			else if (ch == 'n') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == 'j') { // nj 일 경우
+						i++;
+					}
+				}
+			}
+
+			else if (ch == 's') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == '=') { // s= 일 경우
+						i++;
+					}
+				}
+			}
+
+			else if (ch == 'z') {
+				if (i < a.length() - 1) {
+					if (a.charAt(i + 1) == '=') { // z= 일 경우
+						i++;
+					}
+				}
+			}
+
+			count++;
 		}
+
+		System.out.println(count);
 
 	}
 
