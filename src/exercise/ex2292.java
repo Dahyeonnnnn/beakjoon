5e74c5acc6fd0d28aebec003d1cf248bb2c16ac6
andahyeon <adh0618@gmail.com>
2023-02-20 17:11:38 +0900

    추가

--- src/exercise/ex2292.java
+++ src/exercise/ex2292.java
@@ -1,10 +1,28 @@
 package exercise;
 
+import java.util.Scanner;
+
 public class ex2292 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		int count =1;
+		int range=2;
+		
+		if(n==1) {
+			System.out.println(1);
+		}else{
+			while(range<=n) {
+				range=range+(6*count);
+				count++;
+			}
+			System.out.println(count);
+		}
 	}
 
 }
