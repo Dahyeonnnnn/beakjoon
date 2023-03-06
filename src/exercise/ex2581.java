41bd86567c546b41f5db4c0e596cbe0943d3030f
andahyeon <adh0618@gmail.com>
2023-03-06 17:06:56 +0900

    추가

---
+++ src/exercise/ex2581.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class ex2581 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int m = scanner.nextInt();
+		int n = scanner.nextInt();
+		
+		int sum=0;
+		
+		int min=0;
+		
+		for(int i=m;i<n;i++) {
+			
+		}
+	}
+
+}
