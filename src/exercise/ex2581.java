b26f2a10852410dc3aaaedf03bb3bf7a99a1bd52
andahyeon <adh0618@gmail.com>
2023-03-07 17:26:51 +0900

    추가

--- src/exercise/ex2581.java
+++ src/exercise/ex2581.java
@@ -21,7 +21,7 @@ public class ex2581 {
 			if (i == 1) { // 1은 소수가 아님
 				continue;
 			}
-			for (int j = 2; j < i; j++) { // 2~i-1까지의 수랑 i랑 나눠서 나머지가 0이면 소수가 아니므로 for문 스킵.
+			for (int j = 2; j <=  Math.sqrt(i); j++) { // 2~i-1까지의 수랑 i랑 나눠서 나머지가 0이면 소수가 아니므로 for문 스킵.
 				if (i % j == 0) {
 					isPrime = false;
 					break;
