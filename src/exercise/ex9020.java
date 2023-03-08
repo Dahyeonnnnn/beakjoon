b8d6bb0ef8a377187a187b51cad61c195e987671
andahyeon <adh0618@gmail.com>
2023-03-08 17:05:34 +0900

    추가

--- src/exercise/ex9020.java
+++ src/exercise/ex9020.java
@@ -8,8 +8,44 @@ public class ex9020 {
 		// TODO Auto-generated method stub
 
 		Scanner scanner = new Scanner(System.in);
-		
-		int n = scanner.nextInt();
+
+		int T = scanner.nextInt();
+
+		for (int i = 0; i < T; i++) {
+
+
+			int n = scanner.nextInt();
+			
+			int div,rem;
+			div=n/2;
+			rem=n-div;
+			
+			while(true) {
+				if(isPrime(div)&&isPrime(rem)) {
+					break;
+				}else {
+					div--;
+					rem++;
+				}
+			}
+			System.out.println(div+" "+rem);
+
+
+			
+		}
 	}
 
+	//소수확인
+	public static boolean isPrime(int n) {
+		boolean check=true;
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(n%i==0) {
+				check=false;
+				break;
+			}
+		}
+		
+		return check;
+	}
+	
 }
