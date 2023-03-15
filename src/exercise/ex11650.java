318fd4c12346b816859e306a46571dd263387236
andahyeon <adh0618@gmail.com>
2023-03-15 17:08:16 +0900

    추가

---
+++ src/exercise/ex11650.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class ex11650 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			
+		}
+		
+	}
+
+}
