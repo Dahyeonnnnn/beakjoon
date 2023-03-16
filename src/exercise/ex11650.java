8b5c96c0b42ab7dd08244e2b945718e642099b27
andahyeon <adh0618@gmail.com>
2023-03-16 17:30:02 +0900

    11650번 수정

--- src/exercise/ex11650.java
+++ src/exercise/ex11650.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex11650 {
@@ -11,10 +12,25 @@ public class ex11650 {
 		
 		int n = scanner.nextInt();
 		
+		int[][] arr = new int[n][2];
+		
 		for(int i=0;i<n;i++) {
+			arr[i][0] = scanner.nextInt();
+			arr[i][1] = scanner.nextInt();
+			
 			
 		}
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
 		
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
 	}
 
 }
