f07bdd581e6320d0d5fb1226221f2219dd429579
andahyeon <adh0618@gmail.com>
2023-03-23 17:51:23 +0900

    1926번 추가

--- src/algorithm/ex1926.java
+++ src/algorithm/ex1926.java
@@ -5,6 +5,22 @@ public class ex1926 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		1.아이디어 : 
+//		-2중 for=>값1&&방문x
+//		-BFS 돌면서 그림 개수 +1, 최댓값을 갱신
+		
+//		2.시간복잡도:
+//		BFS : O(V+E)
+//		V : 500*500
+//		E : 4*500*500
+//		V*E : 5*250000=100만 <2억 >> 가능!
+		
+//		3.자료구조
+//		그래프 전체 지도 : int[][]
+//		방문 : bool[][]
+//		Queue(BFS)
+		
+		
 	}
 
 }
