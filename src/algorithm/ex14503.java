10d2475618b01e59b01cab0f6b62d8c1e6031874
andahyeon <adh0618@gmail.com>
2023-04-12 17:35:52 +0900

    14503아이디어 추가

--- src/algorithm/ex14503.java
+++ src/algorithm/ex14503.java
@@ -4,7 +4,22 @@ public class ex14503 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		1.아이디어
+//		-특정한 조건을 만족하는 한 계속 이동>while
+//		-4방향 탐색 먼저 수행 > 빈칸이 있을 경우 이동
+//		-4방향 탐색, 뒤로 한칸 가서 반복
+		
+//		2.시간복잡도
+//		while문 최대:n*m
+//		각 칸에서 4방향 연산 수행
+//		
+//		3.자료구조
+//		전체지도:int[][]
+//		내 위치, 방향:int,int,int
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
 	}
 
 }
